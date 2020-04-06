dfa12589743bf03a4328898b0a5c99d00f7e0c0b
pauloricardoaraujo2003 <60099718+pauloricardoaraujo2003@users.noreply.github.com>
2020-04-06 01:17:28 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+var PORT = process.env.PORT || 3000;
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.sendFile(path + "index.html");
+});
+
+app.listen(PORT,function(){
+  console.log("Live at Port" + PORT);
+});
